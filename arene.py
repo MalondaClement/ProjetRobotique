aadda55d9da2264a415ab7e503e7dbecf3f505a7
Clément Malonda <clementmalonda@yahoo.fr>
2019-02-06 18:30:54 +0100

    Update arene.py

--- arene.py
+++ arene.py
@@ -6,7 +6,7 @@ import math as m
 #j'ai juste fait le cas ou la forme est un caree
 class Arene(object):
 #initialise la matrice avec des zero partout
-	def __init__(self,nb_ligne, nb_colonne,list_obs,list_rob): 
+	def __init__(self,nb_ligne, nb_colonne,list_obs,list_rob):
 		self.nb_ligne=nb_ligne
 		self.nb_colonne=nb_colonne
 		self.matrice=np.zeros((nb_ligne,nb_colonne))
@@ -28,7 +28,7 @@ class Arene(object):
 		while i<nb_ligne:
 			self.matrice[i,nb_colonne-1]=1
 			i=i+1
-		
+
 		#mettre un robot dans la matrice
 		for i in list_rob:
 			tmp=0
@@ -50,7 +50,7 @@ class Arene(object):
 					b=i.y-(i.longueur/2)
 					while a<i.x+(i.largeur/2) and not trouv:
 						while b<i.y+(i.longueur/2)and not trouv:
-							if self.matrice[b,a]!=0: 
+							if self.matrice[b,a]!=0:
 								trouv=True
 							b=b+1
 						a=a+1
@@ -90,15 +90,15 @@ class Arene(object):
 							b=b+1
 						a=a+1
 						b=i.y-(i.longueur/2)
-						
-				
+
+
 #on rentre des coordonnee ca nous renvoie le chiffre contenu dans la case de la matrice
 	def get_object(self,x,y):
 		return int(self.matrice[x,y])
 		#0:vide, 1:obstacle, 2:robot
 
-			 
-	
+
+
 #jeu de test:
 #p=Obstacle(6,6,1)
 #m=Obstacle(6,15,1)
@@ -107,7 +107,7 @@ b=Robot(6,8,m.pi/4)
 b.largeur=4
 b.longueur=8
 a=Arene(20,12,[],[b])
-print a.matrice
+print(a.matrice)
 #print a.matrice[-1,-1]
 #a.matrice[0,0]=1
 #print a.matrice
