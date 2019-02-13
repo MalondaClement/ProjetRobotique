ebc1eefadc051efc6989c3089c38a80279cd3879
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2019-02-13 18:59:34 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,5 +1,11 @@
 import math as m
 class Robot(object):
+"""La classe robot permet de créer les robots mais également de gérer ses déplacement (avancer, reculer et rotation).
+La gestion de la détection d'obstacle se fait également dans cette classe.
+	:param x: position en x du centre du robot
+	:param y: position en y du centre du robot
+	:param angle: angle de départ du robot en radian
+"""
 	def __init__(self,x,y,angle):
 		self.x=x
 		self.y=y
@@ -13,13 +19,22 @@ class Robot(object):
 
 
 	def changer_angle(self, delta):
+	""" On peut changer l'angle du robot a chaque instant
+		:param delta: correction d'angle en radian à ajouter à l'angle du robot
+		:returns : rien, changement inplace
+	"""
 	    self.angle+=delta
 	    while self.angle>(m.pi)*2:
-			self.angle-=(m.pi)*2
+			self.angle-=(m.pi)*2 #ici y a pas un problème d'indentation ? 
 	    while self.angle<0 :
-			self.angle+=(m.pi)*2
+			self.angle+=(m.pi)*2 #ici y a pas un problème d'indentation ? 
 
 	def avancer (self, distance):
+	""" On peut faire avancer (et reculer) le robot dans l'arène
+		:param distance: distance que doit parcourir le robot dans la modélisation de l'arène
+			si distance est négatif on avance, sinon on recule
+		:returns : rien, changement inplace
+	"""
 	    self.x+=m.cos(self.angle)*distance
 	    self.x=int(round(self.x,1))
 	    self.y-=m.sin(self.angle)*distance
