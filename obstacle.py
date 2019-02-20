f267c2f91955f9993c325ee0be4938cf7d142ccc
MalondaClement <46990837+MalondaClement@users.noreply.github.com>
2019-02-20 18:44:09 +0100

    Update obstacle.py

--- obstacle.py
+++ obstacle.py
@@ -1,10 +1,10 @@
 class Obstacle(object):
-#""" Obstacle est une classe qui permet de créer des objets dans notre arène
-#    :param x: position en x dans la matrice du centre de l'obstacle
-#    :param y: position en y dans la matrice du centre de l'obstacle
-#    :param forme: permet la gestion de la forme, 1 pour carré, 2 rond, 3 triangle
-#    ...
-#"""
+    """ Obstacle est une classe qui permet de créer des objets dans notre arène
+    :param x: position en x dans la matrice du centre de l'obstacle
+    :param y: position en y dans la matrice du centre de l'obstacle
+    :param forme: permet la gestion de la forme, 1 pour carré, 2 rond, 3 triangle
+    ...
+"""
     def __init__(self, x, y, forme, para1, para2):
         self.x = x
         self.y = y
@@ -23,8 +23,12 @@ class Obstacle(object):
             self.creer_carree(para1,para2)
             
     def creer_carree (self,para1, para2):
+        """Fonction permetant la création d'un obstacle carré
+            :param para1: valeur de la longueur et de la largeur 
+            :param para2: si para1 vaut 0 on prend para2 sinon valeur par defaut 30
+        """
         if para1!=0 and para2!=0:
-            self.largeur = self. longueur = 30
+            self.largeur = self.longueur = 30
             print ("paramètres incorrects :création d'un carré par défaut")
         else:
             if para1!=0 :
@@ -33,6 +37,10 @@ class Obstacle(object):
                 self.longueur = self.largeur= para2
                 
     def creer_rond (self,para1,para2):
+            """Fonction permetant la création d'un obstacle rond
+                :param para1: valeur du rayon 
+                :param para2: si para1 vaut 0 on prend para2 sinon valeur par defaut 10
+            """
             if para1!=0 and para2!=0:
                 self.rayon = 10
                 print ("paramètres incorrects :création d'un rond par défaut")
@@ -43,6 +51,11 @@ class Obstacle(object):
                      self.rayon= para2
                      
     def creer_triangle (self,para1,para2):
+            """Fonction permetant la création d'un obstacle triangulaire
+                :param para1: valeur de la base 
+                :param para2: valeur de la hauteur
+                Si para1 et para2 valent 0, on prend 10 par défaut 
+            """
             if para1==0 or para2==0:
                 self.base = self.hauteur = 10
                 print ("paramètres incorrects :création d'un triangle par défaut")
@@ -51,6 +64,10 @@ class Obstacle(object):
                 self.hauteur=para2
                 
     def creer_rectangle(self,para1,para2):
+            """Fonction permetant la création d'un obstacle carré
+                :param para1: valeur de la longueur et de la largeur 
+                :param para2: si para1 vaut 0 on prend para2 sinon valeur par defaut 30
+            """
             if para1==0 or para2==0:
                 self.longueur=10
                 self.largeur=20
@@ -60,15 +77,15 @@ class Obstacle(object):
                 self.largeur=para2
 
     def get_position(self):
-    #"""Récupération de la position du centre
-    #    :returns: position du centre tuplet
-    #"""
+        """Récupération de la position du centre
+        :returns: position du centre tuplet
+    """
         return self.x, self.y
 
     def get_forme(self):
-    #"""Récupération de la forme
-    #    :returns: position du centre
-    #"""
+        """Récupération de la forme
+        :returns: position du centre
+    """
         return self.forme
 
 
