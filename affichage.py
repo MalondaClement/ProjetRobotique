6f279c0ea320721da3dba9f14c63cb1f348ad7d6
Clément Malonda <clementmalonda@yahoo.fr>
2019-02-21 09:11:51 +0100

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -12,34 +12,34 @@ class Affichage(object):
 		Elle utilise notre matrice pour en faire un représentation concrète dans la fenètre graphique.
 		:param arene: l'arène dont on souhaite avoir la représentation en graphique
 	"""
-    def __init__(self,arene):
-        self.arene=arene
-        self.vitesse=10 
-	
-    def afficher(self):
-    	print (self.arene.matrice)
-    	for i in range(0,self.arene.nb_colonne):
-    		for j in range(0,self.arene.nb_ligne):
-    			if self.arene.matrice[j,i]==1:
-    				zone_dessin.create_rectangle(i,j,i+1,j+1,fill='black')        
-    def afficher_robot(self):
-    	global r
-    	r=zone_dessin.create_polygon(p.x+t*m.cos(p.angle+angle),p.y-t*m.sin(p.angle+angle),p.x+t*m.cos(p.angle-angle),p.y-t*m.sin(p.angle-angle),p.x+t*m.cos(p.angle+angle+m.pi),p.y-t*m.sin(p.angle+angle+m.pi),p.x+t*m.cos(p.angle-angle+m.pi),p.y-t*m.sin(p.angle-angle+m.pi),fill='red',outline='red')
-    	global f
-    	f=zone_dessin.create_line(p.x,p.y,round(50*m.cos(p.angle),1)+p.x,p.y+round(50*m.sin(-p.angle),1),arrow='last',fill='yellow')
-    
-    def zone(self):
-    	global zone_dessin
-    	zone_dessin =Canvas(fenetre, width=self.arene.nb_colonne,height=self.arene.nb_ligne,background='white')
-    	zone_dessin.focus_set()
-    	zone_dessin.bind('<Key>',clavier)
-    	zone_dessin.pack()
+	def __init__(self,arene):
+		self.arene=arene
+		self.vitesse=10
 
-      
-       
+	def afficher(self):
+		print (self.arene.matrice)
+		for i in range(0,self.arene.nb_colonne):
+			for j in range(0,self.arene.nb_ligne):
+				if self.arene.matrice[j,i]==1:
+					zone_dessin.create_rectangle(i,j,i+1,j+1,fill='black')
+	def afficher_robot(self):
+		global r
+		r=zone_dessin.create_polygon(p.x+t*m.cos(p.angle+angle),p.y-t*m.sin(p.angle+angle),p.x+t*m.cos(p.angle-angle),p.y-t*m.sin(p.angle-angle),p.x+t*m.cos(p.angle+angle+m.pi),p.y-t*m.sin(p.angle+angle+m.pi),p.x+t*m.cos(p.angle-angle+m.pi),p.y-t*m.sin(p.angle-angle+m.pi),fill='red',outline='red')
+		global f
+		f=zone_dessin.create_line(p.x,p.y,round(50*m.cos(p.angle),1)+p.x,p.y+round(50*m.sin(-p.angle),1),arrow='last',fill='yellow')
+
+	def zone(self):
+		global zone_dessin
+		zone_dessin =Canvas(fenetre, width=self.arene.nb_colonne,height=self.arene.nb_ligne,background='white')
+		zone_dessin.focus_set()
+		zone_dessin.bind('<Key>',clavier)
+		zone_dessin.pack()
+
+
+
 def dessiner():
-	"""Cette fonction permet de bouger l'image du robot et de sa fleche selon les coordonnée du robot 
-	""" 
+	"""Cette fonction permet de bouger l'image du robot et de sa fleche selon les coordonnée du robot
+	"""
 	zone_dessin.coords(r,p.x+t*m.cos(p.angle+angle),p.y-t*m.sin(p.angle+angle),p.x+t*m.cos(p.angle-angle),p.y-t*m.sin(p.angle-angle),p.x+t*m.cos(p.angle+angle+m.pi),p.y-t*m.sin(p.angle+angle+m.pi),p.x+t*m.cos(p.angle-angle+m.pi),p.y-t*m.sin(p.angle-angle+m.pi))
 	zone_dessin.coords(f,p.x,p.y,round(50*m.cos(p.angle),1)+p.x,p.y+round(50*m.sin(-p.angle),1))
 
@@ -66,21 +66,21 @@ def clavier(event):
 
 arret=False
 def main():
-    global arret
-    if arret==False:
-        if p.distancemax(z.arene):
-            p.changer_angle(m.pi/7)
-        p.avancer(z.vitesse)
-        dessiner()
-        fenetre.after(50,main)
-        
+	global arret
+	if arret==False:
+		if p.distancemax(z.arene):
+			p.changer_angle(m.pi/7)
+		p.avancer(z.vitesse)
+		dessiner()
+		fenetre.after(50,main)
+
 """Les different Boutons"""
 def Demarrer():
-    global arret
-    if arret==True:
-        arret=False
-    main()
-    
+	global arret
+	if arret==True:
+		arret=False
+	main()
+
 
 def reset():
 	"""Cette fonction permet d'effacer un affichage pour pouvoir en importer un autre
@@ -104,81 +104,81 @@ forme(un int)
 FIN"""
 
 def import_file():
-    filepath = askopenfilename(title="Ouvrir un fichier",filetypes=[('txt files','.txt'),('all files','.*')])
-    fichier = open(filepath,'r')
-    ARENE=False
-    ROBOT=False
-    OBSTACLE=False
-    L=[]
-    for i in fichier.readlines():
-    	if i.strip()=="ARENE":
-    		ARENE=True
-    		ROBOT=False
-    		OBSTACLE=False
-    	elif i.strip()=="ROBOT":
-    		b=Arene(int(L[0]),int(L[1]))
-    		b.cree_mur()
-    		L=[]
-    		ARENE=False
-    		ROBOT=True
-    		OBSTACLE=False
-    	elif i.strip()=="OBSTACLE":
-    		global p
-    		p=Robot(int(L[0]),int(L[1]),m.radians(int(L[2])))
-    		global angle
-    		angle=p.calcul_angle()
-    		global t
-    		t=p.calcul_hypo()
-    		b.inserer_robot(p)
-    		L=[]
-    		ARENE=False
-    		ROBOT=False
-    		OBSTACLE=True
-    	elif i.strip()=="FIN":
-    		a=0
-    		while a<len(L):
-    			o=Obstacle(int(L[a]),int(L[a+1]),int(L[a+2]),int(L[a+3]),int(L[a+4]))
-    			b.inserer_obs(o)
-    			a=a+5
-    		global z
-    		z=Affichage(b)
-    		z.arene=b
-    		z.zone()
-    		z.afficher()
-    		z.afficher_robot()
-    	elif ARENE:
-    		L.append(i.strip())
-    	elif ROBOT:
-    		L.append(i.strip())
-    	elif OBSTACLE:
-    		L.append(i.strip())
-    	fichier.close()
+	filepath = askopenfilename(title="Ouvrir un fichier",filetypes=[('txt files','.txt'),('all files','.*')])
+	fichier = open(filepath,'r')
+	ARENE=False
+	ROBOT=False
+	OBSTACLE=False
+	L=[]
+	for i in fichier.readlines():
+		if i.strip()=="ARENE":
+			ARENE=True
+			ROBOT=False
+			OBSTACLE=False
+		elif i.strip()=="ROBOT":
+			b=Arene(int(L[0]),int(L[1]))
+			b.cree_mur()
+			L=[]
+			ARENE=False
+			ROBOT=True
+			OBSTACLE=False
+		elif i.strip()=="OBSTACLE":
+			global p
+			p=Robot(int(L[0]),int(L[1]),m.radians(int(L[2])))
+			global angle
+			angle=p.calcul_angle()
+			global t
+			t=p.calcul_hypo()
+			b.inserer_robot(p)
+			L=[]
+			ARENE=False
+			ROBOT=False
+			OBSTACLE=True
+		elif i.strip()=="FIN":
+			a=0
+			while a<len(L):
+				o=Obstacle(int(L[a]),int(L[a+1]),int(L[a+2]),int(L[a+3]),int(L[a+4]))
+				b.inserer_obs(o)
+				a=a+5
+			global z
+			z=Affichage(b)
+			z.arene=b
+			z.zone()
+			z.afficher()
+			z.afficher_robot()
+		elif ARENE:
+			L.append(i.strip())
+		elif ROBOT:
+			L.append(i.strip())
+		elif OBSTACLE:
+			L.append(i.strip())
+		fichier.close()
 
 def export_file():
 	"""Cette fonction permet de sauvegarder une configuration : creer un nouveau fichier Scenario.txt
 	"""
-    f=open('Scenario.txt','w')
-    f.write('ARENE\n')
-    f.write(str(z.arene.nb_ligne)+'\n')
-    f.write(str(z.arene.nb_colonne)+'\n')
-    f.write("ROBOT\n")
-    for i in z.arene.list_rob:
-    	f.write(str(i.x)+'\n')
-    	f.write(str(i.y)+'\n')
-    	f.write(str(int(m.degrees(i.angle)))+'\n')
-    f.write("OBSTACLE\n")
-    for i in z.arene.list_obj:
-    	f.write(str(i.x)+'\n')
-    	f.write(str(i.y)+'\n')
-    	f.write(str(i.forme)+'\n')
-    	f.write(str(i.para1)+'\n')
-    	f.write(str(i.para2)+'\n')
-    f.write("FIN")
-    f.close()
+	f=open('Scenario.txt','w')
+	f.write('ARENE\n')
+	f.write(str(z.arene.nb_ligne)+'\n')
+	f.write(str(z.arene.nb_colonne)+'\n')
+	f.write("ROBOT\n")
+	for i in z.arene.list_rob:
+		f.write(str(i.x)+'\n')
+		f.write(str(i.y)+'\n')
+		f.write(str(int(m.degrees(i.angle)))+'\n')
+	f.write("OBSTACLE\n")
+	for i in z.arene.list_obj:
+		f.write(str(i.x)+'\n')
+		f.write(str(i.y)+'\n')
+		f.write(str(i.forme)+'\n')
+		f.write(str(i.para1)+'\n')
+		f.write(str(i.para2)+'\n')
+	f.write("FIN")
+	f.close()
 
 def arreter():
-    global arret
-    arret=True
+	global arret
+	arret=True
 
 arret=False
 
