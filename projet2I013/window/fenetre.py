9b2de4a0cf1adf370b6d7cad0e58f9263b71c3d1
Clément Malonda <clementmalonda@yahoo.fr>
2019-02-21 10:08:31 +0100

    Create fenetre.py

---
+++ projet2I013/window/fenetre.py
@@ -0,0 +1,32 @@
+
+
+arret=False ## A VIRER
+"""cree une fenetre"""
+fenetre = Tk()#creer une fenetre
+fenetre.title('Arene')#donner un nom  la fenetre
+
+fenetre.geometry("1200x600")
+"""creation des different bouton"""
+BoutonExporter = Button(fenetre, text ='Exporter', command = export_file)
+BoutonExporter.pack(side = LEFT, padx = 10, pady = 10)
+
+BoutonArreter = Button(fenetre, text ='Arreter', command = arreter)
+BoutonArreter.pack(side = LEFT, padx = 10, pady = 10)
+
+BoutonGo = Button(fenetre, text ='Démarrer', command = Demarrer)
+BoutonGo.pack(side = LEFT, padx = 10, pady = 10)
+
+BoutonQuitter = Button(fenetre, text ='Quitter', command = fenetre.destroy)
+BoutonQuitter.pack(side = LEFT, padx = 5, pady = 5)
+
+BoutonImporter = Button(fenetre, text ='Importer', command = import_file)
+BoutonImporter.pack(side = LEFT, padx = 10, pady = 10)
+
+BoutonReset = Button(fenetre, text ='Reset', command = reset)
+BoutonReset.pack(side = LEFT, padx = 10, pady = 10)
+
+"""creation de case avec des informations a l'interieur"""
+label = Label(fenetre, text="x y", bg="yellow")
+label.pack()
+
+fenetre.mainloop()
