e1da1d82f5353656b8bf23ce21aae25b18cf0cc0
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-02-27 14:47:34 +0100

    Add files via upload

---
+++ controleur_robotreel.py
@@ -0,0 +1,42 @@
+class ControleurRobotReel (object) :
+	def __init__(self, robot) :
+		self.robot = robot
+
+
+	def avancer (vitesse) :
+		# attention : vitesse en mm/s
+		robot.set_motor_dps(3, ((vitesse*360)/robot.WHEEL_CIRCUMFERENCE))
+
+	def tourner (angle) :
+		#attention: angle est une vitesse angulaire en degré par seconde, un angle positif fait tourner dans le sens trigonométrique
+		robot.set_motor_dps(1, -((robot.WHEEL_BASE_CIRCUMFERENCE)/(360/angle)*360)/robot.WHEEL_CIRCUMFERENCE)
+		robot.set_motor_dps(2, ((robot.WHEEL_BASE_CIRCUMFERENCE)/(360/angle)*360)/robot.WHEEL_CIRCUMFERENCE)
+
+
+
+
+
+def clavier(event):
+    """Cette fonction reçoit les touches appuyé par l'utilisateur et effectue des actions pour certaines d'entre elles.
+        :param event: flèche recu
+            -Touche Up permet d'augmenter la vitesse
+            -Touche Down permet baisser la vitesse
+            -Touche Right permet de faire tourner le robot a droite
+            -Touche Left permet de faire tourner le robot a gauche
+    """
+	touche=event.keysym
+	print(touche)
+	if touche =='Up':
+	    avancer(10)
+	if touche =='Down':
+    	avancer(-10)
+	if touche =='Left':
+    	tourner(90)
+    	dessiner()
+	if touche =='Right':
+    	tourner(-90)
+        	dessiner()
+
+    #j'ai repris le fonction clavier de controler sans modifier son architecture
+
+
---
+++ robotreel.py
@@ -0,0 +1,91 @@
+from math import pi,atan,cos,sin,pow,sqrt
+
+WHEEL_BASE_WIDTH         = 117  # distance (mm) de la roue gauche a la roue droite.
+WHEEL_DIAMETER           = 66.5 #  diametre de la roue (mm)
+WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * pi # perimetre du cercle de rotation (mm)
+WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * pi # perimetre de la roue (mm)
+MOTOR_LEFT = 1
+MOTOR_RIGHT = 2
+
+
+class RobotReel(object) :
+
+    def __init__(self,x,y,angle, arene):
+        self.x=x
+        self.y=y
+        self.angle=angle
+        self.largeur=20
+        self.longueur=50
+        self.arene= arene
+        self.MOTOR_LEFT_DPS=0
+        self.MOTOR_RIGHT_DPS=0
+        self.MOTOR_LEFT_ROTATION =0
+        self.MOTOR_RIGHT_ROTATION = 0
+
+
+    def set_motor_dps(self, port,dps):
+        #port=1 pour la roue gauche, 2 pour la roue droite, 3 pour les 2 roues
+        #dps est une vitesse angulaire en degré par seconde
+        if port == MOTOR_LEFT :
+            self.MOTOR_LEFT = dps
+        elif port == MOTOR_RIGHT :
+            self.MOTOR_RIGHT_DPS = dps
+        elif port ==MOTOR_LEFT+MOTOR_RIGHT :
+            self.MOTOR_RIGHT_DPS = self.MOTOR_LEFT_DPS = dps
+
+    def get_motor_position (self) :
+        return MOTOR_LEFT_ROTATION, MOTOR_RIGHT_ROTATION
+
+    def offset_motor_encoder (self, port, offset) :
+        #port=1 pour la roue gauche, 2 pour la roue droite, 3 pour les 2 roues
+        if port == MOTOR_LEFT :
+            self.MOTOR_LEFT_ROTATION = offset
+        elif port == MOTOR_RIGHT :
+            self.MOTOR_RIGHT_ROTATION = offset
+        elif port ==MOTOR_LEFT+MOTOR_RIGHT :
+            self.MOTOR_RIGHT_ROTATION = self.MOTOR_LEFT_ROTATION = offset
+
+    def get_distance (self) :
+        #retourne la distance au plus proche obstacle
+        #je n'ai pas compris les valeurs de l'intervalle de détection du robot -> fonction à préciser 
+        recherche_x= self.x +(self.longueur/2)*cos(angle)
+        recherche_y= self.y -(self.longueur/2)*sin(angle)
+        test=0
+        while test=0:
+            recherche_x+=cos(self.angle)*2
+            recherche_y-=sin(self.angle)*2
+            recherche_x=int(round(recherche_x,0))
+            recherche_y=int(round(recherche_y,0))
+            if recherche_x<0 or recherche_y<0 or recherche_x>self.arene.nb_colonne or recherche_y>self.arene.nb_ligne:
+                test=1
+            if self.arene.matrice[recherche_y, recherche_x]==1:
+                test=1
+        return sqrt(pow(recherche_x-self.x, 2) + pow(recherche_y-self.y, 2))
+
+
+    def stop (self) :
+        #arrête le robot
+        self.MOTOR_LEFT_DPS =MOTOR_RIGHT_DPS = 0
+
+    def actualiser(self) :
+        # réalise une actualisation de la position, ou de l'angle du robot
+        if MOTOR_RIGHT_DPS = MOTOR_LEFT_DPS :
+            x+= cos(angle)* (((MOTOR_LEFT_DPS/20) * WHEEL_DIAMETER )/ 360 ) /10 #conversion en cm
+            y-= sin(angle)* (((MOTOR_LEFT_DPS/20) * WHEEL_DIAMETER )/ 360 ) /10
+
+        elif MOTOR_RIGHT_DPS = -MOTOR_LEFT_DPS :
+            angle+= (MOTOR_RIGHT_DPS*WHEEL_CIRCUMFERENCE/WHEEL_BASE_CIRCUMFERENCE) * × (π/180)
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
