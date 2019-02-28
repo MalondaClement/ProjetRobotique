2dbb239116efdb741e62872da4acc8faaf6eeff4
Clément Malonda <clementmalonda@yahoo.fr>
2019-02-28 09:36:52 +0100

    Update __init__.py

--- projet2I013/window/__init__.py
+++ projet2I013/window/__init__.py
@@ -1,5 +1,5 @@
 from .affichage import Affichage
-from .fenetre import Fenetre
+#from .fenetre import Fenetre
 ##après je sais pas si c'est correct
 from fenetre import Fenetre,demarrer,reset,import_file,export_file,arreter
 from ..modele.arene import Arene,calcul_hypo,calcul_angle
