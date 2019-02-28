13c9439881852bc5029e41a31eaa31f75d837bdd
nagulanselva <46990905+nagulanselva@users.noreply.github.com>
2019-02-28 02:04:49 +0100

    Update TestObstacle.py

--- TestObstacle.py
+++ TestObstacle.py
@@ -1,59 +1,58 @@
 import unittest
-from rep import Obstacle
+from .. import obstacle
 
-class TestObstacle (unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
+	def setUp(self):
+		self.p = obstacle.Obstacle(10,10)
+
 	def test_Obstacle(self):
-		self.assertEqual(Obstacle(10,10,1,10,20).x,10)
-		self.assertEqual(Obstacle(10,10,1,10,20).y,10)
-		self.assertEqual(Obstacle(10,10,1,10,20).forme,1)
-		self.assertEqual(Obstacle(10,10,1,10,20).para1,10)
-		self.assertEqual(Obstacle(10,10,1,10,20).para2,20)
+		self.assertEqual(self.p.x,10)
+		self.assertEqual(self.p.y,10)
 
-	def test_creer_carree(self):
-		self.creer_carree(1,1)
-		self.assertEqual(self.longueur,30)
-		self.assertEqual(self.largeur,30)
-		self.creer_carree(10,0)
-		self.assertEqual(self.longueur,10)
-		self.assertEqual(self.largeur,10)
+
+
+		#self.assertEqual(self.p.forme,1)
+		#self.assertEqual(self.p.para1,10)
+		#self.assertEqual(self.p.para2,20)
+
+
+	'''def test_creer_carree(self):
+		self.p.creer_carree(1,1)
+		self.assertEqual(self.p.longueur,30)
+		self.assertEqual(self.p.largeur,30)
+		self.p.creer_carree(10,0)
+		self.assertEqual(self.p.longueur,10)
+		self.assertEqual(self.p.largeur,10)
 
 	def test_creer_rond(self):
-		self.creer_rond(1,1)
-		self.assertEqual(self.rayon,10)
-		self.creer_rond(10,0)
-		self.assertEqual(self.rayon,10)
-
-	def test_creer_triangle(self):
-		self.creer_triangle(0,0)
-		self.assertEqual(self.base,10)
-		self.assertEqual(self.hauteur,10)
-		self.creer_triangle(20,30)
-		self.assertEqual(self.base,20)
-		self.assertEqual(self.hauteur,30)
+		self.p.creer_rond(1,1)
+		self.assertEqual(self.p.rayon,10)
+		self.p.creer_rond(10,0)
+		self.assertEqual(self.p.rayon,10)
 
 	def test_creer_rectangle(self):
-		self.creer_rectangle(0,0)
-		self.assertEqual(self.longueur,10)
-		self.assertEqual(self.largeur,20)
-		self.creer_rectangle(20,30)
-		self.assertEqual(self.longueur,20)
-		self.assertEqual(self.largeur,30)
-
-	def test_get_position(self):
-		self.assertEqual(Obstacle(10,10,1,10,20).x,(10,10))
-
+		self.p.creer_rectangle(0,0)
+		self.assertEqual(self.p.longueur,10)
+		self.assertEqual(self.p.largeur,20)
+		self.p.creer_rectangle(20,30)
+		self.assertEqual(self.p.longueur,20)
+		self.assertEqual(self.p.largeur,30)
 
 	def test_forme(self):
-		def setUp(self):
-			self.obs=Obstacle(10,10,1,10,20)
-
-		def test_get_forme(self):
-			self.assertEqual(self.obs.get_forme(),1)
+		self.assertEqual(self.p.get_forme(),1)
 
 
+	if __name__ == '__main__':
+		unittest.main(exit=False)'''
+
+class TestStringMethods2(unittest.TestCase):
+	def setUp(self):
+		self.p = a.ObstacleRectangle(10,10,5,20)
 
 
-
-
+	def TestObstacleRectangle(self):
+		self.assertEqual(self.p.hauteur,5)
+		self.assertEqual(self.p.base,20)
+		
 
 
