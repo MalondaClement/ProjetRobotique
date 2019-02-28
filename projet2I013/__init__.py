054cfcebf78707f992ae8d18e0407b3d68b80b15
Clément Malonda <clementmalonda@yahoo.fr>
2019-02-28 09:27:13 +0100

    Update __init__.py

--- projet2I013/__init__.py
+++ projet2I013/__init__.py
@@ -1,1 +1,7 @@
-from .controler import Controler, clavier
+from .controler import Controleur, clavier
+from .modele.arene import Arene
+from .modele.robot import Robot
+from .modele.obstacle import Obstacle
+from .window.affichage import Affichage
+from .window.fenetre import *
+
