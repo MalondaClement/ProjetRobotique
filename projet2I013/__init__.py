4a7e799be82dac0135e434655455d351f487afff
Beaubreuil <46991145+Beaubreuil@users.noreply.github.com>
2019-03-03 15:48:13 +0100

    Delete __init__.py

--- projet2I013/__init__.py
+++
@@ -1,7 +0,0 @@
-from .controler import Controleur, clavier
-from .modele.arene import Arene
-from .modele.robot import Robot
-from .modele.obstacle import Obstacle
-from .window.affichage import Affichage
-from .window.fenetre import *
-
