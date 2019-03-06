a71f7e8435bf43976d6dec88b5a03d8f2c7fd1f1
Beaubreuil <46991145+Beaubreuil@users.noreply.github.com>
2019-03-06 17:21:41 +0100

    Add files via upload

--- projet/demo4.py
+++ projet/demo4.py
@@ -1,11 +1,20 @@
-import math as m
-from interface.fenetre import Fenetre
-from modele.robotreel import RobotReel
-from modele.controleur_robotreel import ControleurRobotReel
+import time
+try:
+    from robot2I013 import Robot2I013 as Robot
+    robot = Robot()
+except ImportError:
+    from interface.fenetre import Fenetre
+    from modele.robotreel import RobotReel as Robot
+    from modele.controleur_robotreel import ControleurRobotReel
+    f=Fenetre()
+    f.creer()
 
-f=Fenetre()
-f.creer()
-
+ctrl=ControleurRobotReel(robot)
+def main(ctrl):
+    while not ctrl.stop():
+        ctrl.update()
+        time.sleep(dt)
+        
 """def main(z,p,f,c):
     p.actualiser()
     z.dessiner()
