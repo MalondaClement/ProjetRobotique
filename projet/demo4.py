1e029adac98ca0cdf504bf270cf4071d297d20d8
Clément Malonda <clementmalonda@yahoo.fr>
2019-03-07 10:13:05 +0100

    Update demo4.py

--- projet/demo4.py
+++ projet/demo4.py
@@ -2,7 +2,7 @@ import time
 try:
     from robot2I013 import Robot2I013 as Robot
     robot = Robot()
-    
+
 except ImportError:
     from interface.fenetre import Fenetre
     from modele.robotreel import RobotReel as Robot
@@ -16,29 +16,3 @@ def main(ctrl):
         ctrl.update()
         time.sleep(0.20)
 main(ctrl)
-    
-"""def main(z,p,f,c):
-    p.actualiser()
-    z.dessiner()
-    if (z.avancer ==1) :
-        if (p.MOTOR_LEFT_ROTATION < 5000*360/p.WHEEL_CIRCUMFERENCE) :
-            c.avancer(1000)
-
-        else :
-            p.stop()
-            z.avancer =0
-            z.tourner =1
-            p.offset_motor_encoder(3, 0)
-    elif (z.tourner ==1) :
-    	if (p.MOTOR_LEFT_ROTATION < p.WHEEL_BASE_CIRCUMFERENCE/4 *360/p.WHEEL_CIRCUMFERENCE ) :
-    		c.tourner(-45)
-    	else :
-    		p.stop()
-    		z.avancer =1
-    		z.tourner =0
-    		p.offset_motor_encoder(3, 0)
-
-while not hasattr(f, 'z'):
-    pass
-c=ControleurRobotReel(f.p)
-main(f.z,f.p,f,c) """   		
