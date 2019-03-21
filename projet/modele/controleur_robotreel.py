0fe27fb522ee2c54ae1022f1eda1297184176b13
nagulanselva <46990905+nagulanselva@users.noreply.github.com>
2019-03-21 10:35:35 +0100

    Add files via upload

--- projet/modele/controleur_robotreel.py
+++ projet/modele/controleur_robotreel.py
@@ -1,27 +1,7 @@
-from .strategie import StratLigne,StratAngleDroit, StratCarre
+from .strategie import StratLigne,StratAngleDroit, StratCarre,StratMur
 from threading import Thread
 class ControleurRobotReel(Thread):
-    def __init__(self,robot):
-        super(ControleurRobotReel,self).__init__()
-        self.robot=robot
-        #self.StratLigne=StratLigne(500, 600, self.robot)
-        self.StratCarre=StratCarre(self.robot,250,500)
-        self.sp=False
 
-    def init(self):
-        self.start()
-
-    def get_distance(self) :
-        return self.robot.get_distance()
-
-    def update(self):
-        if self.StratCarre.step():
-            self.sp=True
-            return self.stop
-
-    def stop(self):
-        return self.sp
-    
     def __init__(self,robot):
         super(ControleurRobotReel,self).__init__()
         self.robot=robot
