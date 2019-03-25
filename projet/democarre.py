35f1bc95f1921acaddf0120677cff8917a29412c
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-03-25 17:05:07 +0100

    Update democarre.py

--- projet/democarre.py
+++ projet/democarre.py
@@ -7,11 +7,11 @@ try:
 except ImportError:
     from interface.fenetre import Fenetre
     from modele.robotreel import RobotReel as Robot
-    from modele.controleur_robotreel import ControleurRobotReel
+    from modele.controleur_robotreel_carre import ControleurRobotReelCarre
     f=Fenetre()
     f.creer()
 
-ctrl=ControleurRobotReel(robot)
+ctrl=ControleurRobotReelCarre(robot)
 def main(ctrl):
     while not ctrl.stop():
         print(robot.get_distance())
