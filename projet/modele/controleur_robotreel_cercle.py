5d232532c0fbdcb0d2e74059c88288b252ee69c4
Clément Malonda <clementmalonda@yahoo.fr>
2019-03-28 12:30:04 +0100

    Update controleur_robotreel_cercle.py

--- projet/modele/controleur_robotreel_cercle.py
+++ projet/modele/controleur_robotreel_cercle.py
@@ -1,18 +1,18 @@
 from .strategie import StratCercle
 from threading import Thread
 class ControleurRobotReelCercle(object):
-	def __init__(self,robot, rayon,temps, direction, cercle):
-       	super(ControleurRobotReelCarre,self).__init__()
-       	self.StratCercle=StratCercle(robot,rayon,temps,direction,cercle)
+    def __init__(self,robot, rayon,temps, direction, cercle):
+            super(ControleurRobotReelCercle,self).__init__()
+            self.StratCercle=StratCercle(robot,rayon,temps,direction,cercle)
 
     def start(self):
-       	self.StratCercle.start()
+        self.StratCercle.start()
 
     def update(self) :
-    	if not self.StratCercle.stop():
-    		self.StratCercle.step()
+        if not self.StratCercle.stop():
+            self.StratCercle.step()
 
     def stop(self):
-    	return self.StratCercle.stop()
+        return self.StratCercle.stop()
 
 
