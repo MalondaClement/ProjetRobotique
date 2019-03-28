bf86ba61637563aa89d0611eacd8d93e4bc75a78
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-03-28 12:18:09 +0100

    Create controleur_robotreel_cercle.py

---
+++ projet/modele/controleur_robotreel_cercle.py
@@ -0,0 +1,18 @@
+from .strategie import StratCercle
+from threading import Thread
+class ControleurRobotReelCercle(object):
+	def __init__(self,robot, rayon,temps, direction, cercle):
+       	super(ControleurRobotReelCarre,self).__init__()
+       	self.StratCercle=StratCercle(robot,rayon,temps,direction,cercle)
+
+    def start(self):
+       	self.StratCercle.start()
+
+    def update(self) :
+    	if not self.StratCercle.stop():
+    		self.StratCercle.step()
+
+    def stop(self):
+    	return self.StratCercle.stop()
+
+
