bde55037181eb1101965141724886bec19f1bbbd
Beaubreuil <46991145+Beaubreuil@users.noreply.github.com>
2019-04-01 17:48:09 +0200

    Add files via upload

--- projet/interface3d/affichage3d.py
+++ projet/interface3d/affichage3d.py
@@ -2,7 +2,6 @@ import pyglet
 import pyglet.gl as pgl
 from pyglet.window import key
 import OpenGL.GL as ogl
-from .affichage3d import *
 from modele.obstacle import*
 import math as m
 
@@ -13,10 +12,10 @@ class Affichage(object):
         self.fenetre3d = fenetre3d
         pgl.glClearColor(0, 0, 0, 1)
         pgl.glEnable(pgl.GL_DEPTH_TEST)
-        self.r=ObstacleRectangle(0,0,50,10)
+        self.r=ObstacleRectangle(0,0,10,50)
 
     def on_draw(self) :
-        z=m.sqrt(self.r.largeur**2+self.r.longueur**2)
+        z=50 #m.sqrt(self.r.largeur**2+self.r.longueur**2)
 
         # Effacer la fenetre
         self.fenetre3d.clear()
@@ -41,8 +40,8 @@ class Affichage(object):
         # Second carre bleu
         pgl.glColor3ub(0, 0, 255)
         pgl.glVertex3f(0, 0, 0)
-        pgl.glVertex3f(0, self.r.longueur, 0)
-        pgl.glVertex3f(0, self.r.longueur, z)
+        pgl.glVertex3f(0, self.r.largeur, 0)
+        pgl.glVertex3f(0, self.r.largeur, z)
         pgl.glVertex3f(0, 0, z)
 
         # Troisieme carre vert
@@ -64,11 +63,11 @@ class Affichage(object):
         pgl.glVertex3f(0, 0, 0)
         pgl.glVertex3f(self.r.longueur, 0, 0)
         pgl.glVertex3f(self.r.longueur, 0, z)
-        pgl.glVertex3f(0, 0, self.r.largeur)
+        pgl.glVertex3f(0, 0, z)
 
         # Sixieme face fonce
         pgl.glColor3ub(40, 40, 40)
-        pgl.glVertex3f(0, 0, self.r.largeur)
+        pgl.glVertex3f(0, 0, z)
         pgl.glVertex3f(self.r.longueur, 0, z)
         pgl.glVertex3f(self.r.longueur, self.r.largeur, z)
         pgl.glVertex3f(0, self.r.largeur, z)
@@ -78,11 +77,53 @@ class Affichage(object):
 
         # Effacer la matrice
         pgl.glPopMatrix()
+    def mur(self):
+        self.fenetre3d.clear()
+        pgl.glPushMatrix()
+        #pgl.glRotatef(self.fenetre3d.xRotation, 0, 1, 0)
+        pgl.glRotatef(self.fenetre3d.yRotation, 0, 1, 1)
+        pgl.glBegin(ogl.GL_QUADS)
+        #bleu
+        pgl.glColor3ub(0, 0, 255)
+        pgl.glVertex3f(-200, 0, -200)
+        pgl.glVertex3f(-200,10, -200)
+        pgl.glVertex3f(200, 10, -200)
+        pgl.glVertex3f(200, 0, -200)
+        #rouge
+        pgl.glColor3ub(255, 0, 0)
+        pgl.glVertex3f(200, 0, -200)
+        pgl.glVertex3f(200,10, -200)
+        pgl.glVertex3f(200, 10, 200)
+        pgl.glVertex3f(200, 0, 200)
+        
+        #vert
+        pgl.glColor3ub(0, 255, 0)
+        pgl.glVertex3f(-200, 0, 200)
+        pgl.glVertex3f(-200,10, 200)
+        pgl.glVertex3f(200, 10, 200)
+        pgl.glVertex3f(200, 0, 200)
+        
+        #jaune
+        pgl.glColor3ub(255, 255, 0)
+        pgl.glVertex3f(-200, 0, -200)
+        pgl.glVertex3f(-200,10, -200)
+        pgl.glVertex3f(-200, 10, 200)
+        pgl.glVertex3f(-200, 0, 200)
+        
+        #blanc
+        pgl.glColor3ub(255, 255, 255)
+        pgl.glVertex3f(-200, 0, 200)
+        pgl.glVertex3f(-200,0, -200)
+        pgl.glVertex3f(200, 0, -200)
+        pgl.glVertex3f(200, 0, 200)
+        
+        
+        pgl.glEnd()
 
     def on_resize(self, width, height):
 
         # Definir le repere (la zone de la fenetre utilisee)
-        pgl.glViewport(0, 0, width, height)
+        #pgl.glViewport(0, 0, width, height)
 
         # Utiliser une projection
         pgl.glMatrixMode(ogl.GL_PROJECTION)
@@ -90,22 +131,9 @@ class Affichage(object):
 
         Ratio = width/height
         pgl.gluPerspective(35, Ratio, 1, 1000)
-        pgl.gluLookAt(0, 0, self.fenetre3d.far, 0, 0, 0, 0, 1, 0)
-
+        pgl.gluLookAt(0, 0, 00, 0, 0, -200, 0, 1, 0)
         pgl.glMatrixMode(ogl.GL_MODELVIEW)
 
         pgl.glTranslatef(0, 0, -400)
 
-
-
-
-    def on_text_motion(self,motion):
-        if motion == key.UP :
-            self.fenetre3d.xRotation -= INCREMENT 
-        elif motion == key.DOWN :
-            self.fenetre3d.xRotation += INCREMENT
-        elif motion == key.LEFT :
-            self.fenetre3d.yRotation -= INCREMENT
-        elif motion == key.RIGHT :
-            self.fenetre3d.yRotation += INCREMENT
 
--- projet/interface3d/fenetre3d.py
+++ projet/interface3d/fenetre3d.py
@@ -3,18 +3,16 @@ import pyglet.gl as pgl
 from pyglet.window import key
 import OpenGL.GL as ogl
 from .affichage3d import *
-from modele.robotreel import *
-from modele.arene import *
 import math as m
 
-WINDOW = 400
+WINDOW = 600
 INCREMENT = 5
 
 class Window(pyglet.window.Window) :
 
     # Rotation au lancement
-    xRotation = yRotation = 30
-    far = 100
+    xRotation = yRotation = 0
+    far = 200
     def __init__(self, width, height, title = '') :
         super(Window, self).__init__( width, height, title)
         pgl.glClearColor(0, 0, 0, 1)
@@ -24,7 +22,7 @@ class Window(pyglet.window.Window) :
 win = Window(WINDOW, WINDOW, 'Cube')
 aff = Affichage(win)
 aff.on_resize(WINDOW,WINDOW)
-aff.on_draw()
+aff.mur()
 
 
 pyglet.app.run()
