a7fc104216269c9e5374bae4f1ea4d1ab957f7ef
Beaubreuil <46991145+Beaubreuil@users.noreply.github.com>
2019-04-03 19:18:37 +0200

    Add files via upload

--- projet/demo3d.py
+++ projet/demo3d.py
@@ -1,1 +1,16 @@
+from interface3d.affichage3d import *
 from interface3d.fenetre3d import *
+from modele.arene import Arene
+from modele.robotreel import RobotReel
+from modele.controleur_robotreel_carre import ControleurRobotReelCarre
+from modele.obstacle import Obstacle
+import math as m
+import pyglet
+WINDOW = 600
+win = Window(WINDOW, WINDOW, 'Cube')
+ar=Arene(500,500)
+rob=RobotReel(70,400,m.pi/2,ar)
+aff = Affichage(win,ar,rob)
+aff.start()
+pyglet.clock.schedule_interval(aff.update, 0.1)
+pyglet.app.run()
