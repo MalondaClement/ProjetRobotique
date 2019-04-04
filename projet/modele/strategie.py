3cefa9d3726a9510290fc56b84e00aaa95f76b3c
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-04-04 10:23:25 +0200

    Update strategie.py

--- projet/modele/strategie.py
+++ projet/modele/strategie.py
@@ -59,6 +59,10 @@ class StratAngle(object):
             self.test=False
 
     def stop(self):
+        if self.parcouru >=self.distance :
+            self.robot.offset_motor_encoder(self.robot.MOTOR_LEFT, self.robot.get_motor_position()[0])
+            self.robot.offset_motor_encoder(self.robot.MOTOR_RIGHT, self.robot.get_motor_position()[1])
+            self.robot.set_motor_dps(self.robot.MOTOR_LEFT+self.robot.MOTOR_RIGHT,0)
         return self.parcouru>=self.distance
 
 class StratCarre(object):
