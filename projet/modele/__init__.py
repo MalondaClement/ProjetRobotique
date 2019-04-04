d938b1257d1881097eb698965ab6da37b50cc9e3
Clément Malonda <clementmalonda@yahoo.fr>
2019-04-04 09:31:49 +0200

    Update __init__.py

--- projet/modele/__init__.py
+++ projet/modele/__init__.py
@@ -4,4 +4,6 @@ from .obstacle import *
 from .robotreel import *
 from .controleur_robotreel_carre import *
 from .controleur_robotreel_mur import *
+from .controleur_robotreel_cercle import *
+from .controleur_robotreel_contourner_porte import *
 
