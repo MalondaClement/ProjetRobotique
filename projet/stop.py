4450b3c3127cfa13aca9109578752478a18385ed
Clément Malonda <clementmalonda@yahoo.fr>
2019-04-04 12:28:45 +0200

    Update stop.py

--- projet/stop.py
+++ projet/stop.py
@@ -6,7 +6,7 @@ try:
     robot.set_motor_dps(robot.MOTOR_LEFT+robot.MOTOR_RIGHT,0)
     robot.offset_motor_encoder(robot.MOTOR_LEFT, robot.get_motor_position()[0])
     robot.offset_motor_encoder(robot.MOTOR_RIGHT, robot.get_motor_position()[1])
-    robot.servo_rotate(90)
+    robot.servo_rotate(30)
 
 except ImportError:
     print("Pas une demo")
