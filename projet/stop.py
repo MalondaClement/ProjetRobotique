ce2759bfa52993f9e12b7a1e05f720ab3dc6dfe5
Clément Malonda <clementmalonda@yahoo.fr>
2019-04-04 11:50:45 +0200

    Create stop.py

---
+++ projet/stop.py
@@ -0,0 +1,12 @@
+import time
+
+try:
+    from robot2I013 import Robot2I013 as Robot
+    robot = Robot()
+    robot.set_motor_dps(robot.MOTOR_LEFT+robot.MOTOR_RIGHT,0)
+    robot.offset_motor_encoder(robot.MOTOR_LEFT, robot.get_motor_position()[0])
+    robot.offset_motor_encoder(robot.MOTOR_RIGHT, robot.get_motor_position()[1])
+    robot.servo_rotate(90)
+
+except ImportError:
+    print("Pas une demo")
