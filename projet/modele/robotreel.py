184745b2d44f7b94405b76e5f8f5b1ce81ee87bc
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-04-04 10:19:23 +0200

    Update robotreel.py

--- projet/modele/robotreel.py
+++ projet/modele/robotreel.py
@@ -112,7 +112,7 @@ class RobotReel(object) :
                 self.angle+= (((self.MOTOR_RIGHT_DPS/20)*self.WHEEL_CIRCUMFERENCE/self.WHEEL_BASE_CIRCUMFERENCE) * (pi/180))
             else :
                 print("entree dans case cercle")
-                print(self.robot.MOTOR_LEFT_DPS+self.robot.MOTOR_RIGHT_DPS)
+                print(self.MOTOR_LEFT_DPS+self.MOTOR_RIGHT_DPS)
                 #rayon= fabs (self.WHEEL_BASE_WIDTH/2*(self.MOTOR_RIGHT_DPS+self.MOTOR_LEFT_DPS)/(self.MOTOR_RIGHT_DPS-self.MOTOR_LEFT_DPS))
                 #vitesserg=self.MOTOR_LEFT_DPS*WHEEL_DIAMETER/360
                 #vitesserd=self.MOTOR_RIGHT_DPS*WHEEL_DIAMETER/360
