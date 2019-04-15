55bb3077e0acc6dac6ae193ef02520295fd7dfca
Clément Malonda <clementmalonda@yahoo.fr>
2019-04-15 16:42:57 +0200

    Update controleur_robotreel_triangle.py

--- projet/modele/controleur_robotreel_triangle.py
+++ projet/modele/controleur_robotreel_triangle.py
@@ -4,7 +4,7 @@ class ControleurRobotReelTriangle(Thread):
     def __init__(self,robot):
         super(ControleurRobotReelTriangle,self).__init__()
         self.robot=robot
-        self.StratTriangle=StratTriangle(self.robot,250,1000)
+        self.StratTriangle=StratTriangle(self.robot,250,100) #ici on ne verra pas le triangle quasiment car notre arene fait 5m par 5m si on veut voir il faut mettre 1000
         self.sp=False
 
     def init(self):
