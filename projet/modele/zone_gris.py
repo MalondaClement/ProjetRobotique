14169ca82c3876c97913bdd77c3031c9e83cc955
Clément Malonda <clementmalonda@yahoo.fr>
2019-04-15 17:43:01 +0200

    Create zone_gris.py

---
+++ projet/modele/zone_gris.py
@@ -0,0 +1,7 @@
+class ZoneGris(object):
+    def __init__(self, x, y,forme):
+        self.x = x
+        self.y = y
+        self.hauteur=10
+        self.largeur=largeur
+        self.longueur=longueur
