ac886c8cc51b6046520eba3799f7b947397b1a2b
NicolasDeschar <46991158+NicolasDeschar@users.noreply.github.com>
2019-05-24 16:06:34 +0200

    Add files via upload

--- projet/interface/fenetre.py
+++ projet/interface/fenetre.py
@@ -102,7 +102,7 @@ class Fenetre(Thread):
                 elif self.i=="1" :
                     self.controleur=ControleurRobotReelMur(self.p)
                 elif self.i=="2" :
-                    self.controleur=ControleurRobotReelCercle(self.p, 100, 10, "droite", 50)
+                    self.controleur=ControleurRobotReelCercle(self.p, 400, 10, "droite", 50)
                 elif self.i=="3" :
                     self.controleur=ControleurRobotReelContournerPorte(self.p)
                 #ajouter nouveaux controleurs ici
